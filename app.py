d586f15429d4387b332ddda1d5c665bb16519b77
mohitkhyalia <70964926+mohitkhyalia@users.noreply.github.com>
2021-06-18 03:14:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-#1
+import smtplib #done
 from flask import Flask , request , render_template , sessions
+from email.mime.text import MIMEText
 #3
 #4
 #5
@@ -17,7 +18,32 @@ def signi():
 @app.route('/blog/kali-install')
 def kali():
     return(render_template('ins-kali.html'))
-#12
+    
+@app.route('/done', methods=['GET','POST'])
+def done():
+    if request.method== 'POST':
+        Sn=request.form["name"]
+        Sm=request.form["email"]
+        ac=request.form["message"]
+        sendid(Sn,Sm,ac)
+        return (render_template("index h.html"))
+
+
+def sendid(username,us,ac):
+    print(username)
+    print(us)
+    b="sender mail: "+us+"   sendername:  "+username+"     msg:"+ac
+    us=MIMEText(b)
+    server=smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    idma='mohitkumar81486@gmail.com'
+    print("login..")
+    
+    myid="mohitkhyalia.yt@gmail.com"
+    server.login("mohitkhyalia.yt@gmail.com","MkMohitkumar81486")
+    server.sendmail(myid,idma,us.as_string())
+    
 #13
 if __name__=="__main__":
     app.run(debug=True)
